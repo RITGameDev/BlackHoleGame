e0d11338a2055967691acc9f32a047657a264334
Ben Hoffman <bah8892@g.rit.edu>
2017-03-14 23:12:29 -0400

    Basic setup for the project
    Created the project
    
    Set the gravity in the Y direction to 0
    Edit > Project Settings > Gravity
    
    Added in a couple circle sprites
    
    Created a test scene, basic folder setup
    
    Created player and blackhole prefabs

---
+++ BlackHoleGame/Assets/_Scripts/BlackHole.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour {
+
+    public float force = 1f;
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    { 
+        // If we are colliding with a player, then push it away
+        if (col.gameObject.CompareTag("Player"))
+        {
+            // Make the player attracted to the blackhole
+
+        }
+    }
+}
---
+++ BlackHoleGame/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class will simply allow player movement 
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 1f;
+
+    private string horizontalInputString = "Horizontal";
+    private string verticalInputString = "Vertical" ;
+
+    private Rigidbody2D rb;
+    private float moveX;
+    private float moveY;
+
+	/// <summary>
+    /// Get the proper components
+    /// </summary>
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	/// <summary>
+    /// Check for input from the player
+    /// </summary>
+	void FixedUpdate ()
+    {
+        // Get the input from the player
+        moveX = Input.GetAxis(horizontalInputString);
+        moveY = Input.GetAxis(verticalInputString);
+
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);
+
+
+    }
+}
